448511cadfc38e92ef9b3e86a0827bc4aee6cd1c
zcylinder <zacharycylinder1@gmail.com>
2014-12-27 15:15:41 -0500

    Update Guess.java

--- Guess.java
+++ Guess.java
@@ -40,5 +40,23 @@
   				guess = (int) (Math.random() * ((upper - lower) + 1)) + lower;
   				
   				System.out.println("Is it " + guess + "? \t)
-  				}
+  				
+  				temp = keyboard.nextLine();
+  				answer = temp.toLowerCase().charAt(0);
+  				
+  				if (answer == 's') lower = guess + 1;  //lower boundary becomes the guess, plus 1
+  				else if (answer == 'b') upper = guess - 1; //upper boundary becomes the guess, minus 1
+  				/**
+  				 *There's no point in including the guessed number in the new boundary, as we know it
+  				 * is not correct
+  				 * /
+  				
+  				count++; 
+  			}
+  		 
+  		 System.out.println();
+  		 System.out.println("I got it after making " + count + " guesses!");
+  	} 
+  				
+  }
   
